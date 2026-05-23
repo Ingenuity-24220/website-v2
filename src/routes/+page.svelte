1996f1d6ffa368590545520fb021689d67d1737a
Niko Yu <137782638+thesleepyniko@users.noreply.github.com>
2026-05-22 22:11:57 -0400

    feat: update images, begin JS code for gallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
-    import teamImg from '$lib/assets/team.png'
-    import arrowUp from '$lib/assets/arrowup.png'
-    import team1 from '$lib/assets/team-1.jpg'
-    import team2 from '$lib/assets/team-2.jpg'
+    import teamImg from '$lib/assets/team.webp'
+    import arrowUp from '$lib/assets/arrowup.webp'
+    import team1 from '$lib/assets/team-1.webp'
+    import team2 from '$lib/assets/team-2.webp'
+    import robot1 from '$lib/assets/robot-1.webp'
+    import robot2 from '$lib/assets/robot-2.webp'
+
+    const members = []
+    let currMemberIndex = $state(0)
+    let memberIndexes = $state([members.length-2, members.length-1, 0, 1, 2]) // cursed? yes. works? hopefully!
+
+    // hooray learning how to use .map (and on the way .forEach :D)
+    function moveMemberRight() {
+        memberIndexes = memberIndexes.map((element) => {
+                return (element + 1 ) % members.length
+            }
+        )
+    }
+
+    function moveMemberLeft() {
+        memberIndexes = memberIndexes.map((element) => {
+                return (element - 1 + members.length) % members.length
+            }
+        )
+    }
 </script>
 
 <div class="hero-bg">
@@ -27,9 +48,9 @@
 </div>
 
 <div class="content">
-    <h2 class="center question">
-        What else is Ingenuity?
-    </h2>
+    <!-- <h2 class="center question">
+        What is Ingenuity?
+    </h2> -->
     <div class="block-grid">
         <div class="left about">
             <h2>Ingenuity is <br><u>from the community.</u></h2>
@@ -39,7 +60,7 @@
                 Our rookie year was 2023, and currently, our team is currently 10 members large.
             </p>
         </div>
-        <div class="right image about">
+        <div class="right image-about">
             <img class="team-img-1" src={team1}>
             <img class="team-img-2" src={team2}>
         </div>
@@ -51,6 +72,10 @@
                 This is our 2025-2026 design! We're in the process of updating our design for <a href="https://www.kyfirstrobotics.org/runfortherobots">Run for The Robots</a>, the premiere event that our team is attending! You can see our robot, Ginny, in the pictures.
             </p>
         </div>
+        <div class="left image-robot">
+            <img class="robot-img-1" src={robot1}>
+            <img class="robot-img-2" src={robot2}>
+        </div>
     </div>
     <div class="block-grid">
         <div class="left team">
@@ -61,11 +86,17 @@
         </div>
     </div>
     <div class="block-grid">
+        <!-- TODO: scrolling member list -->
+    </div>
+    <div class="block-grid">
         <div class="right community">
             <h2>Ingenuity is <br><u>paying it forward.</u></h2>
             <p>
                 From community events to open sourcing our code, Team Ingenuity is dedicated to giving back more to the community than we take.
             </p>
+        </div>
+        <div class="left marquee-community">
+            <!-- TODO: scrolling stuff here, link to seperate page on hover, remember to reduce motion -->
         </div>
     </div>
     <div class="block-grid">
@@ -75,14 +106,20 @@
                 We couldn't do what we do without our sponsors, who provide us money to help us succeed. All of our sponsors for the 25-26 year are on the right!
             </p>
         </div>
+        <div class="right sponsor-image">
+            <!-- TODO: ADD SPONSOR IMAGES HERE -->
+        </div>
     </div> 
     <div class="center contact">
         <h2>Interested?</h2>
         <p>
             We'd be happy to hear from you for anything from sponsorships to questions about what we do. Contact us at the form below, or email <a href="mailto:contact@ingenuity.team">contact@ingenuity.team</a>
         </p>
+        <!-- TODO: ADD FILLOUT HERE -->
     </div>
 </div>
+
+<!-- TODO: FOOTER -->
 
 <style>
 
@@ -91,7 +128,7 @@
         grid-template-columns: repeat(2, 1fr);
         grid-template-rows: 1fr;
         grid-column-gap: 0px;
-        grid-row-gap: 0px;
+        grid-row-gap: 1ch;
     }
     .left {
         display: block;
@@ -109,35 +146,85 @@
 
     .robot, .community {
         grid-column: 2;
+        grid-row: 1;
+    }
+
+    .image-robot, .marquee-community {
+        grid-column: 1;
+        grid-row: 1;
     }
 
     .about {
-        padding-right: 2ch;
+        padding-right: 1ch;
     }
 
     .team-img-1 {
         position: relative;
         rotate: -20deg;
         top: 5%;
-        width: clamp(14rem, 22vw, 24rem);
+        left: 20%;
+        width: clamp(10rem, 16vw, 18rem);
         /* padding-left: 1rem; */
     }
 
     .team-img-2 {
         position: relative;
         top: 10%;
-        left: -10%;
+        left: 5%;
         rotate: 15deg;
-        width: clamp(14rem, 22vw, 24rem);
+        width: clamp(10rem, 16vw, 18rem);
     }
 
-    .image {
+    .robot-img-1 {
+        position: relative;
+        rotate: -20deg;
+        top: -10%;
+        right: -5%;
+        width: clamp(10rem, 16vw, 18rem);
+    }
+    
+    .robot-img-2 {
+        position: relative;
+        top: -15%;
+        rotate: 15deg;
+        width: clamp(10rem, 16vw, 18rem);
+    }
+
+    .image-about {
         display: flex;
         flex-direction: row;
         align-items: center;
         flex-wrap: nowrap;
         gap: 1rem;
-        padding-left: 1rem;
+        padding-left: 2rem;
+        min-width: 0;
+        padding-left: 2ch;
+        padding-bottom: 10ch;
+    }
+
+    .image-about > img {
+        border: solid;
+        border-color: white;
+        border-width: 10px;
+    }
+
+    .image-robot {
+        grid-column: 1;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        flex-wrap: nowrap;
+        gap: 1rem;
+        padding-right: 1rem;
+        min-width: 0;
+        padding-right: 2ch;
+        /* padding-bottom: 10ch; */
+    }
+
+    .image-robot > img {
+        border: solid;
+        border-color: white;
+        border-width: 10px;
     }
 
     .content {
@@ -169,6 +256,10 @@
     }
 
     .block-grid {
+        min-width: 0;
+    }
+
+    .block-grid > * {
         min-width: 0;
     }
 
@@ -284,6 +375,7 @@
         font-family: 'IBM Plex Sans', sans-serif;
         font-weight: 500;
         font-style: normal;
+        background-color: black;
     }
 
     @media (max-width: 48rem) {
@@ -292,6 +384,14 @@
         }
         .robot, .community {
             grid-column: 1;
+            grid-row: auto;
+        }
+        .image-robot, .marquee-community {
+            grid-column: 1;
+            grid-row: auto;
+        }
+        .robot-img-1, .robot-img-2 {
+            left: 0%;
         }
         .left,
         .right {
@@ -300,7 +400,7 @@
         .about {
             padding-right: 0;
         }
-        .image {
+        .image-about, .image-robot {
             justify-content: center;
             padding-left: 0;
             padding-bottom: 0;
